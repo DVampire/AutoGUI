05afe5ea2615e9fb5aedc592a99458a9d8509260
DVampire <1271421361@qq.com>
2023-07-15 21:15:10 +0800

    init

--- tools/scripts.py
+++ tools/scripts.py
@@ -1,6 +1,12 @@
 import pyautogui
+from sam_inference import Inference, show_anns
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
+    device = torch.device("cpu")
+
     pyautogui.hotkey("ctrl", "win", "right")
 
     screenWidth, screenHeight = pyautogui.size()
@@ -9,8 +15,29 @@ def main():
     currentMouseX, currentMouseY = pyautogui.position()
     print(currentMouseX, currentMouseY)
 
-    img = pyautogui.screenshot()
-    img.save('my_screenshot.png')
+    image = pyautogui.screenshot()
+
+    image.save('screen.png')
+
+    image = np.array(image)
+
+    auto_params = dict(
+        points_per_side=32,
+        pred_iou_thresh=0.86,
+        stability_score_thresh=0.92,
+        crop_n_layers=1,
+        crop_n_points_downscale_factor=2,
+        min_mask_region_area=100,  # Requires open-cv to run post-processing
+    )
+
+    infer = Inference(auto_params=auto_params, device=device)
+    masks = infer.infer_auto(image)
+
+    plt.figure(figsize=(20, 20))
+    plt.imshow(image)
+    show_anns(masks)
+    plt.axis('off')
+    plt.savefig("screen_seg.png")
 
 if __name__ == '__main__':
     main()
