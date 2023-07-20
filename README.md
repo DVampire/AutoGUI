a740c5547d80c2404bf73efbe51ed93f1b5a3a98
DVampire <1271421361@qq.com>
2023-07-20 18:06:15 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # AutoGUI
+
+```
+conda create -n autogui python=3.9
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+conda activate autogui
+
+1. download mmocr checkpoints
+cd playground
+
+mkdir checkpoints
+mkdir checkpoints/mmocr
+
+1.1 down dbnet++ checkpoint, URL: https://drive.google.com/file/d/1r3B1xhkyKYcQ9SR7o9hw9zhNJinRiHD-/view?usp=share_link
+mv db_swin_mix_pretrain.pth checkpoints/mmocr
+1.2 down regnet checkpoint
+wget -O checkpoints/mmocr/abinet_20e_st-an_mj_20221005_012617-ead8c139.pth https://download.openmmlab.com/mmocr/textrecog/abinet/abinet_20e_st-an_mj/abinet_20e_st-an_mj_20221005_012617-ead8c139.pth
+
+
+2. download sam checkpoints
+cd sam
+
+mkdir checkpoints
+
+2.1 download sam checkpoint, URL: https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+mv sam_vit_h_4b8939.pth checkpoints/
+
+
+3. pip
+pip install -r requirements.txt
+
+4.run test
+cd tools/
+python mmocr_inference.py
+python sam_inference.py
+python scripts.py
+```
--- tools/scripts.py
+++ tools/scripts.py
@@ -16,7 +16,7 @@ sys.path.append(ROOT)
 from sam_inference import SAMInference, show_anns, show_box, show_mask, show_points
 from mmocr_inference import MMOCRInference
 
-def main():
+def main1():
     device = torch.device("cuda")
 
     pyautogui.hotkey("ctrl", "win", "right")
@@ -29,7 +29,7 @@ def main():
 
     image = pyautogui.screenshot()
 
-    image.save('screen.png')
+    image.save('screen1.png')
 
     image = np.array(image)
 
@@ -49,9 +49,47 @@ def main():
     plt.imshow(image)
     show_anns(masks)
     plt.axis('off')
-    plt.savefig("screen_seg.png")
+    plt.savefig("screen_seg1.png")
 
-    with open("screen.pkl","wb") as op:
+    with open("screen1.pkl","wb") as op:
+        pkl.dump(masks, op)
+
+def main2():
+    device = torch.device("cuda")
+
+    pyautogui.hotkey("ctrl", "win", "right")
+
+    screenWidth, screenHeight = pyautogui.size()
+    print(screenWidth, screenHeight)
+
+    currentMouseX, currentMouseY = pyautogui.position()
+    print(currentMouseX, currentMouseY)
+
+    image = pyautogui.screenshot()
+
+    image.save('screen2.png')
+
+    image = np.array(image)
+
+    auto_params = dict(
+        points_per_side=32,
+        pred_iou_thresh=0.86,
+        stability_score_thresh=0.92,
+        crop_n_layers=1,
+        crop_n_points_downscale_factor=2,
+        min_mask_region_area=100,  # Requires open-cv to run post-processing
+    )
+
+    infer = SAMInference(auto_params=auto_params, device=device)
+    masks = infer.infer_auto(image)
+
+    plt.figure(figsize=(20, 20))
+    plt.imshow(image)
+    show_anns(masks)
+    plt.axis('off')
+    plt.savefig("screen_seg2.png")
+
+    with open("screen2.pkl","wb") as op:
         pkl.dump(masks, op)
 
 def infer_screen1():
@@ -59,22 +97,22 @@ def infer_screen1():
 
     batch_input = []
 
-    image = cv2.imread("screen.png")
+    image = cv2.imread("screen1.png")
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # construct points
-    input_points = []
-    input_labels = []
-    base_point = np.array([[788,1017]])
-    threshold = 15
-    for item in [np.array([i,j]) for i in [threshold] for j in [-threshold, 0, threshold]]:
-        pos = base_point + item
-        input_points.append(pos)
-        input_labels.append([1])
+    # construct box
+    base_point = (788,1017)
+    width = 150
+    height = 30
+    boxes = np.array(
+        [
+            [base_point[0], max(base_point[1] - height, 0), base_point[0] + width, base_point[1]],
+        ],
+    )
 
-    input_boxes = None
-    input_points = np.array(input_points)
-    input_labels = np.array(input_labels)
+    input_boxes = boxes
+    input_points = None
+    input_labels = None
     item = {
         "image": image,
         "input_points": input_points,
@@ -93,13 +131,125 @@ def infer_screen1():
     ax.imshow(batch_input[0]["image"])
     for mask in batched_output[0]['masks']:
         show_mask(mask.cpu().numpy(), ax, random_color=True)
-    for points,labels in zip(batch_input[0]["input_points"], batch_input[0]["input_labels"]):
-        show_points(points, labels, ax)
+    for box in batch_input[0]["input_boxes"]:
+        show_box(box, ax)
     ax.axis('off')
 
     plt.tight_layout()
     plt.show()
 
+def infer_screen2():
+    device = torch.device("cuda")
+
+    batch_input = []
+
+    image = cv2.imread("screen2.png")
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # construct box
+    base_point = (699, 932)
+    width = 500
+    height = 400
+    boxes = np.array(
+        [
+            [base_point[0], max(base_point[1] - height, 0), base_point[0] + width, base_point[1]],
+        ],
+    )
+
+    input_boxes = boxes
+    input_points = None
+    input_labels = None
+    item = {
+        "image": image,
+        "input_points": input_points,
+        "input_labels": input_labels,
+        "input_boxes": input_boxes,
+        "input_masks": None,
+    }
+    batch_input.append(item)
+
+    infer = SAMInference(device=device)
+
+    batched_output = infer.infer_prompt(batch_input, multimask_output=False)
+
+    fig, ax = plt.subplots(1, 1, figsize=(20, 20))
+
+    ax.imshow(batch_input[0]["image"])
+    for mask in batched_output[0]['masks']:
+        show_mask(mask.cpu().numpy(), ax, random_color=True)
+    for box in batch_input[0]["input_boxes"]:
+        show_box(box, ax)
+    ax.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+# 解析图标
+def main3():
+    device = torch.device("cuda")
+
+    # pyautogui.hotkey("ctrl", "win", "right")
+    #
+    # screenWidth, screenHeight = pyautogui.size()
+    # print(screenWidth, screenHeight)
+    #
+    # currentMouseX, currentMouseY = pyautogui.position()
+    # print(currentMouseX, currentMouseY)
+    #
+    # image = pyautogui.screenshot()
+    #
+    # image.save('screen1.png')
+
+    image = cv2.imread("screen1.png")
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = np.array(image)
+
+    auto_params = dict(
+        points_per_side=64,
+        pred_iou_thresh=0.86,
+        stability_score_thresh=0.92,
+        crop_n_layers=1,
+        crop_n_points_downscale_factor=2,
+        min_mask_region_area=100,  # Requires open-cv to run post-processing
+    )
+
+    infer = SAMInference(auto_params=auto_params, device=device)
+    masks = infer.infer_auto(image)
+
+    # 选项栏(0, 966)
+    bottom_base_point = (0, 966)
+    outboxs = [
+        item["bbox"] for item in masks if item["bbox"][1] > bottom_base_point[1]
+    ]
+    outpoints = [
+        np.array([item["bbox"][0] + item["bbox"][2] // 2 , item["bbox"][1] + item["bbox"][3] // 2]) for item in masks if item["bbox"][1] > bottom_base_point[1]
+    ]
+    outlabels = [1 for _ in range(len(outpoints))]
+
+    fig, ax = plt.subplots(1, 1, figsize=(20, 20))
+    plt.imshow(image)
+
+    if not os.path.exists("cut"):
+        os.makedirs("cut")
+
+    count = 0
+    for point, label, box in zip(outpoints, outlabels, outboxs):
+        show_points(point, label, ax)
+
+        cropped_image = image[box[1]:box[1] + box[3], box[0]:box[0] + box[2]]
+        cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(os.path.join("cut", "{:04d}.png".format(count)), cropped_image)
+        count += 1
+
+    plt.axis('off')
+    plt.savefig("screen_seg3.png")
+
 if __name__ == '__main__':
-    # main()
-    infer_screen1()
+    # main1()
+    #infer_screen1()
+
+    # main2()
+    # infer_screen2()
+
+    # 选项栏(0, 966)
+    main3()
